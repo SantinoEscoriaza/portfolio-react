2b986b1175e3c23abcb6fc07455292302330eec4
santino <santinoescoriaza@gmail.com>
2024-07-12 19:48:28 -0300

    Se juntaron todos los elementos correspondientes al cuerpo principal de la pagina en un unico componente Main

---
+++ src/Component/Main/Main.jsx
@@ -0,0 +1,24 @@
+
+import Hero from '../Hero/Hero.jsx'
+import About from '../About/About.jsx'
+import Skills from '../Skills/Skills.jsx'
+import Proyects from '../Proyects/Proyects.jsx'
+import Testimonios from '../Testimonios/Testimonios.jsx'
+import Contact from '../Contact/Contact.jsx'
+
+function Main() {
+
+    
+    return(
+        <main>
+            <Hero />
+            <About />
+            <Skills />
+            <Proyects />
+            <Testimonios />
+            <Contact />
+        </main>
+    )
+}
+
+export default Main
--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './normalize.css'
 import './index.css'
 import Header from './Component/Header/Header.jsx'
-import Hero from './Component/Hero/Hero.jsx'
-import About from './Component/About/About.jsx'
-import Skills from './Component/Skills/Skills.jsx'
-import Proyects from './Component/Proyects/Proyects.jsx'
-import Testimonios from './Component/Testimonios/Testimonios.jsx'
-import Contact from './Component/Contact/Contact.jsx'
+import Main from './Component/Main/Main.jsx'
 import Footer from './Component/Footer/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <Hero />
-    <About />
-    <Skills />
-    <Proyects />
-    <Testimonios />
-    <Contact />
+    <Main />
     <Footer />
   </React.StrictMode>,
 )
