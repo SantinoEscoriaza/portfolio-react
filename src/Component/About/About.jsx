cb5766082276547dd4013f0b8fb1743a29623c0f
santino <santinoescoriaza@gmail.com>
2024-07-12 17:33:04 -0300

    Se agrego el codigo html y css correspondiente a la seccion About. Tambien se movio el codigo que corresponde a los botones de distintas secciones al archivo index.css, ya que este codigo es compartido por varias secciones

---
+++ src/Component/About/About.jsx
@@ -0,0 +1,28 @@
+import './About.css'
+
+const About = () => {
+
+
+    return(
+    <section class="about" id="aboutid">
+        <h2>Sobre mi</h2>
+        <img src="img/imgAbout.jpeg" alt="foto"/>
+        <section class="textAbout">
+            <p class="descripcion">Tengo 18 años y soy de Argentina. Tengo experiencia en el desarrollo de páginas web. Estudio Ingeniería en sistemas en la Universidad Tecnológica Nacional, y tengo mucho interés en el mundo de la programación y el manejo de datos.</p>
+            <p class="descripcion">Estoy capacitado en el desarrollo de sitios web desde 0, manejando tecnologias de front-end y back-end que se complementan con el manejo de ciertas herramientas de diseño grafico para poder ofrecer un servicio lo mas completo</p>
+        </section>
+        {/* <!-- BOTONES --> */}
+        <a href=""><button class="botonRojo">Descargar CV</button></a>
+        <a href="#contactid"><button class="botonRojo">Contrátame</button></a>
+        {/* <!-- ICONOS --> */}
+        <section class="icons">
+            <a href="https://www.facebook.com" target="_blank"><i class="bi bi-facebook redSocialIcon"></i></a>
+            <a href="https://www.instagram.com" target="_blank"><i class="bi bi-instagram redSocialIcon"></i></a>
+            <a href="https://www.x.com" target="_blank"><i class="bi bi-twitter redSocialIcon"></i></a>
+            <a href="https://www.linkedin.com/in/santino-escoriaza-6833b8311/" target="_blank"><i class="bi bi-linkedin redSocialIcon"></i></a>
+        </section>
+    </section>
+    )
+}
+
+export default About
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Component/Header/Header.jsx'
 import './normalize.css'
 import './index.css'
+import Header from './Component/Header/Header.jsx'
 import Hero from './Component/Hero/Hero.jsx'
+import About from './Component/About/About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <Hero />
+    <About />
   </React.StrictMode>,
 )
