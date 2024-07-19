bbfecd79df1b2be4905d5e56f74729640ddbc0fc
santino <santinoescoriaza@gmail.com>
2024-07-19 13:16:35 -0300

    Se cambio el nombre de main.jsx a body.jsx para distinguirlo del Main como componente. Tambien se agregó ChakraProvider para usar los estilos del paquete

--- src/body.jsx
+++ src/body.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import './normalize.css'
 import './index.css'
 import Header from './Component/Header/Header.jsx'
@@ -8,8 +9,10 @@ import Footer from './Component/Footer/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <Main />
-    <Footer />
+    <ChakraProvider>
+      <Header />
+      <Main />
+      <Footer />
+    </ChakraProvider>
   </React.StrictMode>,
 )
