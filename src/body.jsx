82f6dc1dbe62d51d9fa5e5de0e547e9411e6469e
santino <santinoescoriaza@gmail.com>
2024-07-24 19:34:20 -0300

    Debug: Se agrego un archivo theme.jsx para color el modo claro de Chakra-UI como el modo por defecto y evitar que este se modifique segun el dispositivo. Esto con el fin de que no se modifiquen estilos que no han sido configurados para ambos modos de color

--- src/body.jsx
+++ src/body.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import './normalize.css'
 import './index.css'
 import Header from './Component/Header/Header.jsx'
 import Main from './Component/Main/Main.jsx'
 import Footer from './Component/Footer/Footer.jsx'
+import theme from './assets/theme.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Header />
       <Main />
       <Footer />
